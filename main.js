1fcd683d6cfa874473f029a009c48f3494e17502
JTDoe <Jordendoerflinger@gmail.com>
2022-05-14 22:55:50 -0500

    first commit

--- main.js
+++ main.js
@@ -6,12 +6,25 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+let str = "i am a String"
 
 
-
-const countIt = (str) => {
-  console.log('BOOMS', str.length)
+const countChar = (str) => {
+  let re = /[^a-z]/g
+ let newstr = str.replace( /[^A-Za-z0-9]/g, '')
+  const counts = {};
+  for (const s of newstr) {
+    if (counts[s]) {
+      counts[s]++
+    } else {
+      counts[s] = 1
+    }
+  }
+  return counts;
 }
+
+const newstr = "The quick brown fox jumps over the lazy dog and the sleeping cat early in the day.";
+console.log(countChar(newstr))
 
 
 const getPrompt = () =>  {
@@ -31,3 +44,4 @@ if (typeof describe === 'function') {
 } else {
   getPrompt();
 }
+
